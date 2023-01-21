faff9266d34626c7505888dd897b856eaeccff50
Nate Braukhoff <braukhoff96@gmail.com>
2023-01-21 12:11:59 -0600

    Able to get questions and categories

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = config = {
+	user: 'sa',
+	password: 'Password5!',
+	server: 'localhost', 
+	database: 'SelfAssessmentSurvey',
+	options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+}
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 5000
 const repository = require('./repository')
-
-var config = {
-	user: 'sa',
-	password: 'Password5!',
-	server: 'localhost', 
-	database: 'SelfAssessmentSurvey',
-	options: {
-        encrypt: false,
-        trustServerCertificate: true
-    }
-};
-
-repository.GetCategories(config)
-    .then(categories => console.log(categories))
-repository.GetQuestions(config)
-    .then(questions => console.log(questions))
+const config = require('./config')
 
 
 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
+app.get('/GetCategories', async (req, res) => {
+    let categories = await repository.GetCategories(config)
+    res.send(categories)
+})
+
+app.get('/GetQuestions', async (req, res) => {
+    let questions = await repository.GetQuestions(config)
+    res.send(questions)
+})
 
